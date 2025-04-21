3c30001550b6d7924c0e7e5dac7c555ca86bf1d2
GitHub Action <action@github.com>
2025-04-21 11:05:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/lyra95/jenkinsdocs/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/lyra95/jenkinsdocs/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.lyra95.jenkinsdocs.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
